f97f3bc071dab75ccbfa9d64d98733c0e6958708
Rash Estebek <98695442+RashEstebek@users.noreply.github.com>
2023-11-14 18:40:16 +0600

    exercise5 is done

--- exercise5/problem8/index.ts
+++ exercise5/problem8/index.ts
@@ -1,5 +1,49 @@
-async function fetchCharacterWithMovies() {
-  // Your code
+// type Person = {
+//   name: string;
+//   films: string[];
+// }
+
+async function fetchCharacterWithMovies(characterId: number) {
+  const api = "https://swapi.dev/api";
+  const characterUrl = `${api}/people/${characterId}/`;
+
+  try {
+    const characterResponse = await fetch(characterUrl);
+
+    if (!characterResponse.ok) {
+      throw new Error("smth went wrong");
+    }
+
+    const characterData = await characterResponse.json();
+    const films = await Promise.all(
+      characterData.films.map(async (filmUrl: string) => {
+        const filmResponse = await fetch(filmUrl);
+
+        if (!filmResponse.ok) {
+          throw new Error("smth went wrong");
+        }
+
+        const filmData = await filmResponse.json();
+        return filmData.title;
+      })
+    );
+
+    return {
+      name: characterData.name,
+      films,
+    };
+  } catch (error) {
+    return Promise.reject(new Error("smth went wrong"));
+  }
 }
+
+fetchCharacterWithMovies(1)
+// {
+//   name: "Luke Skywalker",
+//   films: ["A New Hope", "The Empire Strikes Back", "Return of the Jedi", "Revenge of the Sith"]
+// }
+
+fetchCharacterWithMovies(150)
+// Error: smth went wrong
 
 export default fetchCharacterWithMovies;
